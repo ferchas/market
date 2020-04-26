016dc15cebacbfeaa75b80fc2492e2e24f21e3a6
Fernando De Caro <you@example.com>
2020-04-26 13:51:07 -0300

    add img not found  for each item

--- src/App/components/items/index.js
+++ src/App/components/items/index.js
@@ -10,8 +10,8 @@ const Items = ({ list = []}) => {
         <a href="#" className="items-wp--liked">
           <img className="items-wp--icon" src="./icon/favorite_border-24px.svg"/>
         </a>
-        <div  className="items-wp__img-wp" >
-          <img  className="items-wp__img-wp-el" src="./img/items/screen1.png" />
+        <div  className="items-wp__img-box" >
+          <img  className="items-wp__img-box--src" src={elem.img || './img/not-photo@2x.svg'} />
         </div>
         <h4 className="items-wp__price">$9800</h4> 
         <p className="items-wp__discribe">Alambre Galvanizado Calibre 12 por Kilo</p>
